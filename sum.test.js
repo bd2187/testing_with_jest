2651cc49030201c399c544d820e034489e51bbc0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-27 10:26:29 -0700

    add testing for object assignment

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,19 @@
 import sum from './sum';
 
 test('adds 1 + 2 to equal 3', () => {
+  // toBe utilizes "==="
   expect(sum(1, 2)).toBe(3);
+});
+
+test('object assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test('object assignment', () => {
+  // use toEqual when testing arrays/objects
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toBe({ one: 1, two: 2 });
 });
