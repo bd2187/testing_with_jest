822b73250bf358f08e609f1fbca3de8143ddbab5
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-27 10:52:24 -0700

    Create tests for null and numbers

--- sum.test.js
+++ sum.test.js
@@ -6,14 +6,36 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('object assignment', () => {
+  // use toEqual when testing arrays/objects
+  // toEqual recursively checks every field of an object or array.
   const data = { one: 1 };
   data['two'] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
 
-test('object assignment', () => {
-  // use toEqual when testing arrays/objects
-  const data = { one: 1 };
-  data['two'] = 2;
-  expect(data).toBe({ one: 1, two: 2 });
+test('adding positive numbers is not zero', () => {
+  // test for opposite of matcher
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).toBeFalsy();
+});
+
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+
+  expect(value).toEqual(4);
+  expect(value).toBe(4);
 });
