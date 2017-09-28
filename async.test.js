f7e2d51fd417b1b95e766b95af06ae7d4b7768aa
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-27 21:00:32 -0700

    Add required 'done' argument to test callback in async.test

--- async.test.js
+++ async.test.js
@@ -1,11 +1,8 @@
-const bar = function(n, cb) {
-  setTimeout(function() {
-    cb(n);
-  }, 1000);
-};
+const bar = (n, cb) => setTimeout(() => cb(n), 2000);
 
-test('bar', function() {
+test('bar', done => {
   bar(7, function(n) {
     expect(n).toBe(7);
+    done();
   });
 });
