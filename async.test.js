47e8402dd19eb98ddcc6d1177f441bc7c751b46d
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-09-27 21:21:46 -0700

    Test promises

--- async.test.js
+++ async.test.js
@@ -1,8 +1,34 @@
-const bar = (n, cb) => setTimeout(() => cb(n), 2000);
+/*
+callbacks
+*/
 
-test('bar', done => {
+const bar = (n, cb) => setTimeout(() => cb(n), 1000);
+
+test('bar', function(done) {
   bar(7, function(n) {
     expect(n).toBe(7);
     done();
+  });
+});
+
+/*
+promises
+*/
+
+test('promise', done => {
+  Promise.resolve(7)
+    .then(n => {
+      expect(n).toBe(7);
+    })
+    .then(done)
+    .catch(done.fail);
+});
+
+test('another promise', () => {
+  // rather than using 'done' argument, you can return the Promise
+
+  expect.assertions(1);
+  return Promise.resolve('peanut butter').then(data => {
+    expect(data).toBe('peanut butter');
   });
 });
